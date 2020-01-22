4f8a02ef73dc8ec7502960083cd6602ba6681be2
veselin0 <vesselin60@gmail.com>
2020-01-22 16:02:01 +0100

    Updates

---
+++ precache-manifest.44e19fa8d8161d2145afabe9094e9a4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b9559043046805cfef4fc5aa55d3f6",
+    "url": "/ou-calculator/index.html"
+  },
+  {
+    "revision": "dcce6b8d224a585f8ee5",
+    "url": "/ou-calculator/static/js/2.e7d4c519.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ou-calculator/static/js/2.e7d4c519.chunk.js.LICENSE"
+  },
+  {
+    "revision": "829dee81be2dc1947898",
+    "url": "/ou-calculator/static/js/main.0069dec4.chunk.js"
+  },
+  {
+    "revision": "054ecce987c433e9a661",
+    "url": "/ou-calculator/static/js/runtime-main.866eaeb2.js"
+  }
+]);
